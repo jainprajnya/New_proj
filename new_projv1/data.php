9e15ab1871fea25227ca1c1c8c7edbdb3415dfb2
Pragya Jain <pragya.jain@olacabs.com>
2014-05-16 10:13:50 +0530

    new porj

---
+++ new_projv1/data.php
@@ -0,0 +1,13 @@
+<?php
+// $con = mysql_connect("localhost","root","root");
+// if($con ==FALSE){
+// throw new Exception("Could not connect to Database, Reason: " . mysql_error());}
+
+// mysql_select_db("blackbox_prod", $con);
+
+  // $arr=array("data"=>array([0]=>array("name"=>"overview","graphId"=>"b1d1feb2-007f-4179-a4d4-db150b2d262b","attributeList"=>array(0,1,2),"filterList"=>array(3)),
+		// 				   [1]=>array("name"=>"overview2","graphId"=>"b1d1feb2-007f-4179-a4d4-db150b2d262b","attributeList"=>array(0,1,2),"filterList"=>array(3))
+  // 						));
+
+$arr=array("data"=>array("name"=>"overview","graphId"=>"b1d1feb2-007f-4179-a4d4-db150b2d262b","attributeList"=>array(0,1,2),"filterList"=>array(3)));
+?> 
---
+++ new_projv1/top_pannel.php
@@ -0,0 +1,6 @@
+<ul id="menu">
+	<li ><a href="#" class="level1"><span>Statistics</span></a></li>
+	<li ><a href="#" class="level1"><span>Profile</span></a></li>
+	<li ><a href="#" class="level1"><span>Q & A</span></a></li>
+	<li ><a href="#" class="level1"><span>Help</span></a></li>
+</ul>
