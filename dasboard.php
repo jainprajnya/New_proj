a2fe76c6a1022c261c0e788a98ceb960f88f7aa5
Pragya Jain <pragya.jain@olacabs.com>
2014-07-04 20:53:37 +0530

    adding js file in git

---
+++ dasboard.php
@@ -0,0 +1,39 @@
+<div class="opaque">   
+<div id="avg_rating"></div> 
+<div id="net_promoter">
+    <table class="net_promoter_table">
+        <tr><th style="text-align: center; font-size: 24px; padding: 10px 10px;">NPS</th></tr>
+        <tr><td style="text-align: center; font-size: 24px; padding: 10px 10px;"><canvas id="rect4"></canvas></tr>
+    </table>
+</div>
+<div id="nps_parameters">
+<table class="nps_params_table">
+    <tr><td >Promoters</td>
+        <td><canvas id="rect1"> 10 </canvas></td>
+    </tr>
+    <tr><td >Detractors</td>
+        <td><canvas id="rect2"> 20 </canvas></td>
+    </tr>
+    <tr> <td>passive</td>
+        <td><canvas id="rect3"> 40 </canvas></td>
+    </tr>
+    </table>
+
+</div>
+<div id="stats_summary_overall">
+    <table id="stats_overall">
+        <tr><th style="padding: 10px 80px;">Overall</th></tr>
+        <tr><td style=" padding: 10px 80px;">ABC</td></tr>
+        <tr><td style=" padding: 10px 80px;">XYZ</td></tr>
+    </table>
+</div>
+<div id="stats_summary_today">
+    <table id="stats_today">
+        <tr><th style="padding: 10px 80px;">Today</th></tr>
+        <tr><td style="padding: 10px 80px;">PQR</td></tr>
+        <tr><td style="padding: 10px 80px;">LMO</td></tr>
+    </table>
+</div>
+
+</div>
+<div id="dummy_div" style="height: 80px; width:100%;background-color: white; display: hidden;"></div>
