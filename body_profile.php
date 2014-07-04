05262de7576890e631efa9aacc870b4ff767d39d
Pragya Jain <pragya.jain@olacabs.com>
2014-07-04 22:11:47 +0530

    css edit

--- body_profile.php
+++ body_profile.php
@@ -1,15 +1,12 @@
-<div class="opaque">
-
-</div>
 <div class="content">
         <div class="blog">
 
             <div class="blog-header">
                 <div class="container">
-                    <a class="fancy-font" href="?cat=fashion"> Offer & Info</a>
-                    <a class="fancy-font" href="?cat=fashion">Q & A</a>
-                    <a class="fancy-font" href="?cat=fashion">Update Profile</a>
-                    <a class="fancy-font" href="?cat=fashion">Licensing</a>
+                    <a class="fancy-font" id="offers" href="#"> Offer & Info</a>
+                    <a class="fancy-font" id="questions" href="#">Q & A</a>
+                    <a class="fancy-font" id="profile" href="#">Update Profile</a>
+                    <a class="fancy-font" id="license" href="#">Licensing</a>
                     
                     <div class="no-padding col-md-1 col-sm-1 col-xs-1">
                         <a class="rss-icon hidden" href="http://rss.com" target="_blank"><span>Rss</span></a>
@@ -17,7 +14,8 @@
                 </div>
             </div>
             <div class="container">
-               
+               <div id="profile" class="questions_list">
+               </div>
             </div>
 
             <a class="show-more fancy-font" href="#">
